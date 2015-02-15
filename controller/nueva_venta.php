cbd6f07e6cde56c4b867695642122bd4f20132db
NeoRazorX <neorazorx@gmail.com>
2015-02-15 20:23:24 +0100

    Actualizadas las extensiones.

--- controller/nueva_venta.php
+++ controller/nueva_venta.php
@@ -1,8 +1,8 @@
 <?php
 /*
  * This file is part of FacturaSctipts
- * Copyright (C) 2014  Carlos Garcia Gomez  neorazorx@gmail.com
- * Copyright (C) 2014  Francesc Pineda Segarra  shawe.ewahs@gmail.com
+ * Copyright (C) 2014-2015  Carlos Garcia Gomez  neorazorx@gmail.com
+ * Copyright (C) 2014-2015  Francesc Pineda Segarra  shawe.ewahs@gmail.com
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
