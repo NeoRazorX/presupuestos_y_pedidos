1f935d3c561e1c173d0c7da0ae34e6e7c254ee0e
NeoRazorX <neorazorx@gmail.com>
2015-09-26 12:01:56 +0200

    Los pedidos de proveedor se llaman ahora pedidos de compra.

--- controller/compras_pedidos.php
+++ controller/compras_pedidos.php
@@ -32,7 +32,7 @@ class compras_pedidos extends fs_controller
 
    public function __construct()
    {
-      parent::__construct(__CLASS__, ucfirst(FS_PEDIDOS).' a proveedor', 'compras');
+      parent::__construct(__CLASS__, ucfirst(FS_PEDIDOS).' de compra', 'compras');
    }
 
    protected function private_core()
