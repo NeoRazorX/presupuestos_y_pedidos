a26c9913d2c8976d86848b55d7886d5ad6dc8b5f
NeoRazorX <neorazorx@gmail.com>
2015-10-28 21:08:29 +0100

    - Mejoras en las vistas de presupuestos y pedidos aprobados.

--- model/linea_pedido_cliente.php
+++ model/linea_pedido_cliente.php
@@ -165,6 +165,11 @@ class linea_pedido_cliente extends fs_model
       return $this->pvptotal*(100+$this->iva-$this->irpf+$this->recargo)/100;
    }
    
+   public function descripcion()
+   {
+      return nl2br($this->descripcion);
+   }
+   
    public function show_codigo()
    {
       $codigo = 'desconocido';
