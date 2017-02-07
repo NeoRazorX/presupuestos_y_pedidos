9714faf43c28db34b366d7e72337635a46b1d277
NeoRazorX <neorazorx@gmail.com>
2017-02-07 23:53:30 +0100

    Solucionados bugs al hacer copias de pedidos o presupuestos del año anterior.

--- controller/ventas_pedido.php
+++ controller/ventas_pedido.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * This file is part of FacturaScripts
- * Copyright (C) 2014-2016  Carlos Garcia Gomez       neorazorx@gmail.com
+ * Copyright (C) 2014-2017  Carlos Garcia Gomez       neorazorx@gmail.com
  * Copyright (C) 2014-2015  Francesc Pineda Segarra   shawe.ewahs@gmail.com
  *
  * This program is free software: you can redistribute it and/or modify
@@ -172,6 +172,13 @@ class ventas_pedido extends fs_controller
       if($this->pedido->idoriginal)
       {
          $pedi->idoriginal = $this->pedido->idoriginal;
+      }
+      
+      /// enlazamos con el ejercicio correcto
+      $ejercicio = $this->ejercicio->get_by_fecha($pedi->fecha);
+      if($ejercicio)
+      {
+         $pedi->codejercicio = $ejercicio->codejercicio;
       }
       
       if( $pedi->save() )
--- controller/ventas_presupuesto.php
+++ controller/ventas_presupuesto.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * This file is part of FacturaScripts
- * Copyright (C) 2014-2016  Carlos Garcia Gomez       neorazorx@gmail.com
+ * Copyright (C) 2014-2017  Carlos Garcia Gomez       neorazorx@gmail.com
  * Copyright (C) 2014-2015  Francesc Pineda Segarra   shawe.ewahs@gmail.com
  *
  * This program is free software: you can redistribute it and/or modify
@@ -187,6 +187,13 @@ class ventas_presupuesto extends fs_controller
       if($this->presupuesto->idoriginal)
       {
          $presu->idoriginal = $this->presupuesto->idoriginal;
+      }
+      
+      /// enlazamos con el ejercicio correcto
+      $ejercicio = $this->ejercicio->get_by_fecha($presu->fecha);
+      if($ejercicio)
+      {
+         $presu->codejercicio = $ejercicio->codejercicio;
       }
       
       if( $presu->save() )
